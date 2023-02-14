2c4786275a380161576b0cfc7d65648b66820dd9
HonorThe1nsane <troymd2009@hotmail.com>
2023-02-13 17:54:52 -0700

    Update validation

--- controllers/validation.js
+++ controllers/validation.js
@@ -5,11 +5,17 @@ const carValidation = (req, res, next) => {
 
 
     check('carMake', 'Car make is required').not().isEmpty().trim().escape();
+    console.log('carMake has been validated');
     check('carModel', 'Car model is required').not().isEmpty().trim().escape();
+    console.log('carModel has been validated');
     check('engineSize', 'Engine size is required').not().isEmpty().trim().escape();
+    console.log('engineSize has been validated');
     check('favoriteColor', 'Color is required').not().isEmpty().trim().escape();
+    console.log('favoriteColor has been validated');
     check('year', 'Year is required').not().isEmpty().trim().escape();
+    console.log('Year has been validated');
     check('price', 'Price is required').not().isEmpty().trim().escape();
+    console.log('Price has been validated');
 
     check(req.body, 'Request body cannot be empty').not().isEmpty();
 
@@ -26,7 +32,7 @@ const carValidation = (req, res, next) => {
 const personValidation = (req, res, next) => {
 
     check('firstName', 'First Name is required').not().isEmpty().trim().escape();
-    console.log( `"${param}" First name checked`);
+    console.log( `First name checked`);
     check('lastName', 'Last Name is required').not().isEmpty().trim().escape();
     console.log('Last name checked');
     check('email', 'Email is required').not().isEmpty().trim().escape();
