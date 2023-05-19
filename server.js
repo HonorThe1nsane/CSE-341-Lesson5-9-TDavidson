b508048d36c242c824bbe080c3949ba807437eab
HonorThe1nsane <troymd2009@hotmail.com>
2023-05-19 17:04:25 -0600

    fix

--- server.js
+++ server.js
@@ -12,60 +12,6 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-const { OAuth2Client } = require('google-auth-library');
-const axios = require('axios');
-const { auth } = require('express-openid-connect');
-
-// Create a new OAuth2Client instance
-const oAuth2Client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_OAUTH_REDIRECT_URL
-);
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.GOOGLE_CLIENT_SECRET,
-    baseURL: `http://localhost:8080`,
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    issuerBaseURL: process.env.GOOGLE_OAUTH_REDIRECT_URL
-};
-
-
-const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: process.env.SCOPES,
-});
-
-
-
-// Redirect the user to the authentication URL
-app.get('/login', (req, res) => {
-    res.redirect(authUrl);
-});
-
-// Exchange the authorization code for an access token
-// app.get('/oauth2callback', async (req, res) => {
-//     const { code } = req.query;
-
-//     try {
-//         const { tokens } = await oAuth2Client.getToken(code);
-//         oAuth2Client.setCredentials(tokens);
-
-//         // Use the access token to make API requests
-//         const response = await axios.get('https://www.googleapis.com/drive/v3/files', {
-//             headers: { Authorization: `Bearer ${oAuth2Client.credentials.access_token}` },
-//         });
-
-//         console.log(response.data);
-//         res.send(response.data);
-//     } catch (error) {
-//         console.error(error);
-//         res.send('Error!');
-//     }
-// });
-
 
 app
     .use(auth(config))
