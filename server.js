6800d1913538b0743d656065f6d4fbe7acfdfda8
HonorThe1nsane <troymd2009@hotmail.com>
2023-06-09 21:41:38 -0600

    updated with oauth 2.0

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const express = require('express');
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2').Strategy;
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -12,6 +14,33 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+// Configure OAuth 2.0
+passport.use(
+    'oauth2',
+    new OAuth2Strategy(
+        {
+            authorizationURL: process.env.AUTH_URL,
+            tokenURL: process.env.TOKEN_URL,
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: process.env.CALLBACK_URL,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOrCreate({ oauthId: profile.id }, (err, user) => {
+                user.accessToken = accessToken;
+                user.save((err) => {
+                    done(null, user);
+                });
+            });
+        }
+    )
+);
+
+// Middleware for initializing Passport
+app.use(passport.initialize());
+
+// Add the OAuth 2.0 authentication middleware
+app.use('/api', passport.authenticate('oauth2', { session: false }));
 
 app
     .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
